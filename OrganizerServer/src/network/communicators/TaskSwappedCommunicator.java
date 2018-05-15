f4d71a9908f54f78e2aa4fe9f7d39a22ad926cb9
Bernat Rovirosa Roca <bernat.rovirosa.2016@salleurl.edu>
2018-05-15 19:53:18 +0200

    [CORE] S'ha afegit un nou ENUM i s'ha creat 3 Communicators més: SWAP_TASK, DELETE_USER i INVITE_USER.

---
+++ OrganizerServer/src/network/communicators/TaskSwappedCommunicator.java
@@ -0,0 +1,27 @@
+package network.communicators;
+
+import db.DataBaseManager;
+import model.project.Task;
+import network.Communicable;
+import network.DedicatedServer;
+import network.DedicatedServerProvidable;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * S'encarrega de la comuncació quan s'intercanvia la posició de dues tasques.
+ * Notifica a tots els clients del projecte.
+ */
+public class TaskSwappedCommunicator implements Communicable {
+    @Override
+    public void communicate(DedicatedServer ds, DedicatedServerProvidable provider) {
+        final ArrayList<Task> tasks;
+        try {
+            tasks = (ArrayList<Task>) ds.readData();
+            DataBaseManager.getTaskDBManager().swapTask(tasks);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
