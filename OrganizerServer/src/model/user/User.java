f702dbdf0722fc5993d87ce47bb9611b50c77bcf
Bernat Rovirosa Roca <bernat.rovirosa.2016@salleurl.edu>
2018-05-16 18:08:56 +0200

    [BUG] S'ha igualat el User amb el del client

--- OrganizerServer/src/model/user/User.java
+++ OrganizerServer/src/model/user/User.java
@@ -1,6 +1,7 @@
 package model.user;
 
 import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
 public class User implements Serializable {
@@ -17,6 +18,21 @@ public class User implements Serializable {
 
     public String getUserName() {
         return userName;
+    }
+
+    public String getMD5(String entry) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(entry.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
