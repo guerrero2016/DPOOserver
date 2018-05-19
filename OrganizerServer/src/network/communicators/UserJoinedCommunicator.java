f890d8a74e151250fc08b5c7e53d7f68d7d1a8b5
Guillem Guerrero <guille19981998@gmail.com>
2018-05-19 18:48:17 +0200

    "[CORE] Funcio per saber si existeix Project amb el user ja afegit"

--- OrganizerServer/src/network/communicators/UserJoinedCommunicator.java
+++ OrganizerServer/src/network/communicators/UserJoinedCommunicator.java
@@ -1,13 +1,16 @@
 package network.communicators;
 
 import db.DataBaseManager;
+import db.DataManagers.ProjectDBManager;
 import model.ServerObjectType;
 import model.project.Project;
+import model.user.User;
 import network.Communicable;
 import network.DedicatedServer;
 import network.DedicatedServerProvidable;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * S'encarrega de la comunicació quan un client entra a un projecte mitjançant el seu ID.
@@ -28,5 +31,31 @@ public class UserJoinedCommunicator implements Communicable {
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+
+    /**
+     * Funció que comprova si un usuari ja forma part d'un projecte.
+     * @param projectId Identficador del projecte que volem comprovar.
+     * @param ds DedicatedServer d'on treurem el nom de l'usuari a comprovar.
+     * @return Si ja està al Project retornarem 1, sinó 0.
+     */
+    public int checkIfAlreadyJoined(String projectId, DedicatedServer ds){
+        int exists = 0;
+        String userId;
+        //recuperem el projecte del que volem comprovar si l'usuari ja en forma part.
+        Project projectToCheck = DataBaseManager.getInstance().getProjectDBManager().getProject(projectId);
+        //D'aquest proecte recuperem tots els usuaris que en formen part.
+        ArrayList<User> usersToCheck = projectToCheck.getUsers();
+        //Treiem el User a comprovar
+        userId = ds.getUsername();
+        //Per cada usuari que forma part del Projecte mirarem si coincideix amb l'usuari a comprovar
+        for(User u : usersToCheck){
+            if(u.equals(userId)){
+                exists = 1;
+                return exists;
+            }
+        }
+        return exists;
     }
 }
