ccd1bf1297cd02c3d4469ad60c9a79b8b47f8d3e
christian98gm <c.garcia.2016@salleurl.edu>
2018-05-20 20:50:31 +0200

    [CORE] c.garcia.2016 Task finishing

--- OrganizerServer/src/network/communicators/TaskNotDoneCommunicator.java
+++ OrganizerServer/src/network/communicators/TaskNotDoneCommunicator.java
@@ -8,18 +8,14 @@ import network.DedicatedServerProvidable;
 
 import java.io.IOException;
 
-/**
- * S'encarrega de la comunicació quan una tasca s'ha acabat.
- * Notifica a tots els clients del projecte.
- */
-public class TaskDoneCommunicator implements Communicable {
+public class TaskNotDoneCommunicator implements Communicable {
     @Override
     public void communicate(DedicatedServer ds, DedicatedServerProvidable provider) {
         try {
             String categoryId = ds.readData().toString();
             final String taskID = ds.readData().toString();
-            DataBaseManager.getInstance().getTaskDBManager().taskDone(taskID);
-            provider.sendBroadcast(ds.getHash(), ServerObjectType.TASK_DONE, categoryId);
+            DataBaseManager.getInstance().getTaskDBManager().taskNotDone(taskID);
+            provider.sendBroadcast(ds.getHash(), ServerObjectType.TASK_NOT_DONE, categoryId);
             provider.sendBroadcast(ds.getHash(), null, taskID);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
